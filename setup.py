bcaf5f486985be6d34107218a91f94bc5af8fdbc
Greg Lucas <greg.m.lucas@gmail.com>
2019-11-09 08:48:35 -0700

    Updating setup.py file

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ setup(
     # },
     install_requires=REQUIRED,
     include_package_data=True,
-    license='ISC',
+    license='MIT',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -100,6 +100,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
