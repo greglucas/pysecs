fixed minus sign for eqn a.8

pySECS/secs.py
@@ -276,7 +276,7 @@ def T_df(obs_loc, sec_loc):
         Br2 = mu0*x/(4*np.pi*obs_r) * (1./np.sqrt(1 - 2*x*cos_theta + x**2) - 1)
 
         # Amm & Viljanen: Equation A.8
-        Btheta2 = mu0/(4*np.pi*obs_r)*((obs_r-sec_r*cos_theta) /
+        Btheta2 = - mu0/(4*np.pi*obs_r)*((obs_r-sec_r*cos_theta) /
                                        np.sqrt(obs_r**2 - 2*obs_r*sec_r*cos_theta + sec_r**2) - 1)
         Btheta2 = np.divide(Btheta2, sin_theta, out=np.zeros_like(sin_theta), where=sin_theta != 0)
 