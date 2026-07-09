fee64d784a95ba20c8538f999b93db60181a9574
GitHub Action <action@github.com>
2026-07-09 03:26:19 +0000

    Update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0.dev1+unknown.g89cf81962',
+    VERSION: '0.1.0.dev1+unknown.g75096ec59',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
